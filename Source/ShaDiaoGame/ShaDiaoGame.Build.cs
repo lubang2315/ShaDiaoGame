470dab5c0472789175ea6f87404047b0dea553f3
main <xiaofang@666>
2026-02-26 19:47:25 +0800

    初步完成了GAS搭建

--- Source/ShaDiaoGame/ShaDiaoGame.Build.cs
+++ Source/ShaDiaoGame/ShaDiaoGame.Build.cs
@@ -8,9 +8,9 @@ public class ShaDiaoGame : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "GameplayTags", "GameplayAbilities", "GameplayTasks" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] { "AnimGraphRuntime" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
