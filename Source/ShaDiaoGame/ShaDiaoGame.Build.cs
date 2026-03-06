4355226c3410a37ce7f3006509e1565a274e65f0
main <xiaofang@666>
2026-03-07 01:08:18 +0800

    完成敌人AI以及敌人向玩家移动

--- Source/ShaDiaoGame/ShaDiaoGame.Build.cs
+++ Source/ShaDiaoGame/ShaDiaoGame.Build.cs
@@ -10,7 +10,7 @@ public class ShaDiaoGame : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "GameplayTags", "GameplayAbilities", "GameplayTasks" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] { "AnimGraphRuntime" });
+		PrivateDependencyModuleNames.AddRange(new string[] { "AnimGraphRuntime", "AIModule"});
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
